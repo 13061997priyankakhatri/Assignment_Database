3f405850f30a031d24fe0344d1a83bafbdc512c4
13061997priyankakhatri <137032972+13061997priyankakhatri@users.noreply.github.com>
2023-10-19 20:12:30 +0530

    Update Student-and-Exam-table.sql

--- Student-and-Exam-table.sql
+++ Student-and-Exam-table.sql
@@ -1,19 +1,23 @@
 use student;
+
 create table student1(
 roll_no int primary key,
 name varchar(50),
 branch varchar(20)
 );
+
 create table exam(
 roll_no int ,
 s_code varchar(50),
 marks int,
 P_code varchar(50)
 );
+
 insert into student1 values
 (1,"Jay","Computer Science"),
 (2,"Suhani","Electronic and Comp"),
 (3,"kriti","Electronic and Comp");
+
 insert into exam values
 (1,"CS11",50,"CS"),
 (1,"CS12",60,"CS"),
@@ -21,8 +25,6 @@ insert into exam values
 (2,"EC102",70,"EC"),
 (3,"EC101",45,"EC"),
 (3,"EC102",50,"EC");
+
 select * from student1;
 select * from exam;
-select * from student1 left join exam on student1.roll_no=exam.roll_no
-union 
-select * from student1 right join exam on student1.roll_no=exam.roll_no;
